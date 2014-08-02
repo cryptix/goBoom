2a08f0834f3852dfd1ecda0fa4f434352dfd03e9
Henry <cryptix@riseup.net>
2014-08-03 01:37:33 +0200

    Revert "using io.Pipe for upload, content length still incorrect... :/"
    This reverts commit 9b5c29ea94a6c42947ecd8e3a35c2289a6c46356.

--- fileSystem.go
+++ fileSystem.go
@@ -1,13 +1,10 @@
 package goBoom
 
 import (
-	"fmt"
+	"bytes"
 	"io"
 	"mime/multipart"
-	"net/http"
 	"path/filepath"
-
-	"github.com/cheggaaa/pb"
 )
 
 type FilesystemService struct {
@@ -26,80 +23,44 @@ func newFilesystemService(c *Client) *FilesystemService {
 }
 
 // Upload
-func (s *FilesystemService) Upload(fname string, input io.Reader, size int64) (resp *http.Response, newInfo *ItemInfo, err error) {
+func (s *FilesystemService) Upload(fname string, input io.Reader) (int, *ItemInfo, error) {
 
-	pipeOut, pipeIn := io.Pipe()
-	bar := pb.New(int(size)).SetUnits(pb.U_BYTES)
-	bar.ShowSpeed = true
-
-	writer := multipart.NewWriter(pipeIn)
-
-	// do the request concurrently
-	// var item ItemInfo
-	done := make(chan error)
-	go func() {
-
-		// prepare request
-		req, err := s.c.NewReaderRequest("POST", "ul", pipeOut, writer.FormDataContentType())
-		if err != nil {
-			done <- err
-			return
-		}
-		// TODO calculate right amount of overhead
-		req.ContentLength = size // filesize
-		req.ContentLength += 227 // multipart header exclouding filename
-		req.ContentLength += int64(len(fname))
-		req.ContentLength -= 19
-
-		// set  token
-		q := req.URL.Query()
-		q.Set("token", s.c.User.session)
-		q.Set("parent", "1")
-		req.URL.RawQuery = q.Encode()
-
-		fmt.Println("Created Request")
-		bar.Start()
-
-		var fInfo ItemInfo
-		// resp, err = s.c.c.Do(req)
-		resp, err = s.c.DoJson(req, &fInfo)
-		if err != nil {
-			done <- err
-			return
-		}
-		newInfo = &fInfo
-
-		done <- nil
-	}()
+	var bodyBuf bytes.Buffer
+	writer := multipart.NewWriter(&bodyBuf)
 
 	part, err := writer.CreateFormFile("file", filepath.Base(fname))
 	if err != nil {
-		return
+		return 0, nil, err
 	}
 
-	out := io.MultiWriter(part, bar)
-	_, err = io.Copy(out, input)
+	_, err = io.Copy(part, input)
 	if err != nil {
-		fmt.Println("pipe copy error")
-		err = <-done
-		return
+		return 0, nil, err
 	}
 
 	err = writer.Close()
 	if err != nil {
-		return
+		return 0, nil, err
 	}
 
-	err = pipeIn.Close()
+	// prepare request
+	req, err := s.c.NewReaderRequest("POST", "ul", &bodyBuf, writer.FormDataContentType())
 	if err != nil {
-		return
+		return 0, nil, err
 	}
 
-	err = <-done
-	bar.Finish()
+	// set  token
+	q := req.URL.Query()
+	q.Set("token", s.c.User.session)
+	q.Set("parent", "1")
+	req.URL.RawQuery = q.Encode()
+
+	// do the request
+	var item ItemInfo
+	resp, err := s.c.DoJson(req, &item)
 	if err != nil {
-		return
+		return resp.StatusCode, nil, err
 	}
 
-	return
+	return resp.StatusCode, &item, nil
 }
